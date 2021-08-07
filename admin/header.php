438ea4c43b98a3a5b42dbcd86207ff4a7e4ee0e9
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2021-08-07 13:26:47 +0600

    Update header.php
    login feature update

--- admin/header.php
+++ admin/header.php
@@ -1,4 +1,13 @@
 <!DOCTYPE html>
+<?php
+
+session_start();
+
+if(!isset($_SESSION['username'])){
+    header ('Location: http://localhost/news-template/admin/');
+}
+
+?>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
@@ -26,8 +35,8 @@
                     </div>
                     <!-- /LOGO -->
                       <!-- LOGO-Out -->
-                    <div class="col-md-offset-9  col-md-1">
-                        <a href="logout.php" class="admin-logout" >logout</a>
+                    <div class="col-md-offset-9  col-md-3">
+                        <a href="logout.php" class="admin-logout" >Hello <?php echo $_SESSION['username'] ?>, logout</a>
                     </div>
                     <!-- /LOGO-Out -->
                 </div>
@@ -43,12 +52,20 @@
                             <li>
                                 <a href="post.php">Post</a>
                             </li>
+
+                            <?php
+                            if($_SESSION['role'] == '1'){
+
+                            
+                            ?>
                             <li>
                                 <a href="category.php">Category</a>
                             </li>
                             <li>
                                 <a href="users.php">Users</a>
                             </li>
+
+                            <?php } ?>
                         </ul>
                     </div>
                 </div>
