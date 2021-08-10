0cac4db512d07febc47b8316fb122ff82ac9f889
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2021-08-10 22:01:55 +0600

    Create delete-post.php

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -9,10 +9,18 @@ if($_SESSION['role'] == '0'){
 
 include 'config.php';
 $user_id = $_GET['id'];
+$cat_id = $_GET['catid'];
 
-$sql = "DELETE FROM post WHERE post_id = {$user_id}";
+$sql1 = "SELECT * FROM post WHERE post_id = {$user_id}";
+$result1 = mysqli_query($conn, $sql1);
+$row = mysqli_fetch_assoc($result1);
 
-if(mysqli_query($conn, $sql)){
+unlink("upload/".$row['post_img']);
+
+$sql = "DELETE FROM post WHERE post_id = {$user_id};";
+$sql .= "UPDATE category SET post= post - 1 WHERE category_id = {$cat_id}";
+
+if(mysqli_multi_query($conn, $sql)){
     header ('Location: http://localhost/news-template/admin/post.php');
 }
 ?>
