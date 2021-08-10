aabb24e6981c18a1bd343fe568fa7b5037195397
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2021-08-11 00:21:48 +0600

    Update delete-post.php

--- admin/delete-post.php
+++ admin/delete-post.php
@@ -17,6 +17,8 @@ $row = mysqli_fetch_assoc($result1);
 
 unlink("upload/".$row['post_img']);
 
+//unlink use to delete from folder not from database
+
 $sql = "DELETE FROM post WHERE post_id = {$user_id};";
 $sql .= "UPDATE category SET post= post - 1 WHERE category_id = {$cat_id}";
 
