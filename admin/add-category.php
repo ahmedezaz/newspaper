aa13f34d1b3bdc34cb8bdb6f0a008cd301284596
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2021-08-11 21:53:10 +0600

    Update add-category.php

--- admin/add-category.php
+++ admin/add-category.php
@@ -6,11 +6,36 @@
                   <h1 class="admin-heading">Add New Category</h1>
               </div>
               <div class="col-md-offset-3 col-md-6">
+
+              <?php
+                
+
+               
+                if(isset($_POST['save'])){
+                    $conn = mysqli_connect('localhost', 'root', '', 'news-site');
+            
+                    //$cat_id = mysqli_real_escape_string($conn,$_POST['category_id']);
+                    $cat_name = mysqli_real_escape_string($conn,$_POST['category_name']);
+                  
+            
+                        $sql1 = "INSERT INTO category(category_name) VALUES ('{$cat_name}')";
+                        if(mysqli_query($conn,$sql1)){
+                            header ('Location: http://localhost/news-template/admin/category.php');
+                        }else {
+                            echo "SQL ERROR";
+                        }
+                       
+                    
+            
+                }
+
+
+              ?>
                   <!-- Form Start -->
-                  <form action="" method="POST" autocomplete="off">
+                  <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST" autocomplete="off">
                       <div class="form-group">
                           <label>Category Name</label>
-                          <input type="text" name="cat" class="form-control" placeholder="Category Name" required>
+                          <input type="text" name="category_name" class="form-control" placeholder="Category Name" required>
                       </div>
                       <input type="submit" name="save" class="btn btn-primary" value="Save" required />
                   </form>
