ae6accf52212ac3e1039838055c21cb158fb5901
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2021-08-15 11:41:19 +0600

    Update post.php

--- admin/post.php
+++ admin/post.php
@@ -23,12 +23,12 @@
             $offset = ($page - 1) * $limit;
 
             if($_SESSION['role'] == '1'){
-                $sql = "SELECT post.post_id, post.title, post.description, post.post_date, category.category_name, user.username,post.category FROM post
+                $sql = "SELECT post.post_id, post.title, post.description, post.post_date, category.category_name, user.username, post.category FROM post
                     LEFT JOIN category ON post.category = category.category_id
                     LEFT JOIN user ON post.author = user.user_id
                     ORDER BY post.post_id DESC LIMIT {$offset}, {$limit}";
             }elseif($_SESSION['role'] == '0'){
-                $sql = "SELECT post.post_id, post.title, post.description, post.post_date, category.category_name, user.username,post.category FROM post
+                $sql = "SELECT post.post_id, post.title, post.description, post.post_date, category.category_name, user.username, post.category FROM post
                     LEFT JOIN category ON post.category = category.category_id
                     LEFT JOIN user ON post.author = user.user_id
                     WHERE post.author ={$_SESSION['user_id']}
@@ -64,7 +64,7 @@
                               <td><?php echo $row['post_date'] ?></td>
                               <td><?php echo $row['username'] ?></td>
                               <td class='edit'><a href='update-post.php?id=<?php echo $row['post_id'] ?>'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-post.php?id=<?php echo $row['post_id'] ?>&catid=<?php echo $row['post.category']?>'><i class='fa fa-trash-o'></i></a></td>
+                              <td class='delete'><a href='delete-post.php?id=<?php echo $row['post_id']; ?>&catid=<?php echo $row['post.category']; ?>'><i class='fa fa-trash-o'></i></a></td>
                           </tr>
                           
                       </tbody>
