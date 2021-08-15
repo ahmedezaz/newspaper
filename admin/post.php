7ad554dcfee897bd0dee87c566504de4f6790e34
Ezaz Ahmed <ahmedezaz@users.noreply.github.com>
2021-08-15 11:54:42 +0600

    Update post.php

--- admin/post.php
+++ admin/post.php
@@ -64,7 +64,8 @@
                               <td><?php echo $row['post_date'] ?></td>
                               <td><?php echo $row['username'] ?></td>
                               <td class='edit'><a href='update-post.php?id=<?php echo $row['post_id'] ?>'><i class='fa fa-edit'></i></a></td>
-                              <td class='delete'><a href='delete-post.php?id=<?php echo $row['post_id']; ?>&catid=<?php echo $row['post.category']; ?>'><i class='fa fa-trash-o'></i></a></td>
+                            // delete paarmeter post.category not working thats why use only category like $row['category']  
+                            <td class='delete'><a href='delete-post.php?id=<?php echo $row['post_id']; ?>&catid=<?php echo $row['category']; ?>'><i class='fa fa-trash-o'></i></a></td>
                           </tr>
                           
                       </tbody>
